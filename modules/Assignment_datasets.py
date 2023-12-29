5db6b38b8a9e8e7ddf1e79fab64006a53f76d939
Ivan <152974976+IvanLusso@users.noreply.github.com>
2023-12-29 12:30:46 +0100

    Changes on datasets functions
    started cleaning function of appearance
    minor changes in Appearance, Games and Player_valuations

--- modules/Assignment_datasets.py
+++ modules/Assignment_datasets.py
@@ -1,37 +1,44 @@
 import pandas as pd
 
 
-def get_appearance(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/appearances.csv')
+def get_appearance(location='', apply_func=lambda x: x):
+    return apply_func(pd.read_csv(location + 'Assignment_Data_2023-2024/appearances.csv'))
 
 
-def get_club_games(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/club_games.csv')
+def get_club_games(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/club_games.csv')
 
 
-def get_clubs(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/clubs.csv')
+def get_clubs(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/clubs.csv')
 
 
-def get_competitions(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/competitions.csv')
+def get_competitions(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/competitions.csv')
 
 
-def get_game_events(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/game_events.csv')
+def get_game_events(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/game_events.csv')
 
 
-def get_game_lineups(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/game_lineups.csv')
+def get_game_lineups(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/game_lineups.csv')
 
 
-def get_games(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/games.csv')
+def get_games(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/games.csv')
 
 
-def get_player_valuations(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/player_valuations.csv')
+def get_player_valuations(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/player_valuations.csv')
 
 
-def get_players(s=''):
-    return pd.read_csv(s + 'Assignment_Data_2023-2024/players.csv')
+def get_players(location=''):
+    return pd.read_csv(location + 'Assignment_Data_2023-2024/players.csv')
+
+
+def clean_appearance(appearance):
+    appearance['date'] = pd.to_datetime(appearance['date'])
+    appearance['competition_id'] = appearance['competition_id'].astype('string')
+    appearance.dropna(inplace=True)#uncomplete
+    return appearance
