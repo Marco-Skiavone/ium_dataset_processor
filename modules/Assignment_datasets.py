f58a212735f416944373ecd8b21a7e67237fb1cd
Marco Schiavone <marko.skiaa40@gmail.com>
2023-12-30 12:33:48 +0100

    I made the club_games function for cleaning
    WARNING!! I still have to add the column from games.csv!!

--- modules/Assignment_datasets.py
+++ modules/Assignment_datasets.py
@@ -40,5 +40,16 @@ def get_players(location=''):
 def clean_appearance(appearance):
     appearance['date'] = pd.to_datetime(appearance['date'])
     appearance['competition_id'] = appearance['competition_id'].astype('string')
-    appearance.dropna(inplace=True)#uncomplete
+    appearance.dropna(inplace=True)  # @Todo uncomplete
     return appearance
+
+
+def clean_club_games(club_games):
+    club_games = club_games[['game_id', 'club_id', 'own_goals', 'own_position',
+                             'own_manager_name', 'hosting', 'is_win']]
+    club_games['own_position'] = club_games['own_position'].apply(lambda x: int(-1 if x != x else int(x)))
+    club_games['own_manager_name'] = club_games['own_manager_name'].astype('string')
+    club_games['hosting'] = club_games['hosting'].apply(lambda x: bool(x.__str__() == 'Home'))
+    club_games['is_win'] = club_games['is_win'].apply(lambda x: bool(x == 1))
+    # @Todo Here we have to add 'club_formation' column from games!
+    return club_games
