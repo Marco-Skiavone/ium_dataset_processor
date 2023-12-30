515c43b48afc3e1d336596dd91f460ed8a6d7b5f
Marco Schiavone <marko.skiaa40@gmail.com>
2023-12-30 16:52:40 +0100

    Writing clubs function. Still not tested!!

--- modules/Assignment_datasets.py
+++ modules/Assignment_datasets.py
@@ -40,7 +40,7 @@ def get_players(location=''):
 def clean_appearance(appearance):
     appearance['date'] = pd.to_datetime(appearance['date'])
     appearance['competition_id'] = appearance['competition_id'].astype('string')
-    appearance.dropna(inplace=True)  # @Todo uncomplete
+    appearance.dropna(inplace=True)  # @Todo uncompleted
     return appearance
 
 
@@ -51,20 +51,33 @@ def clean_club_games(club_games):
     club_games['own_manager_name'] = club_games['own_manager_name'].astype('string')
     club_games['hosting'] = club_games['hosting'].apply(lambda x: bool(x.__str__() == 'Home'))
     club_games['is_win'] = club_games['is_win'].apply(lambda x: bool(x == 1))
+
     # @Todo Here we have to add 'club_formation' column from games!
+    # club_formation is divided into 2 columns: home_c. and away_c.
+    # Every club_games tuple is linked up to only 1 value.
     return club_games
 
 
 def clean_clubs(clubs):
-    clubs = clubs.drop(columns=['coach_name', 'total_market_value'])    # Both are null
-    clubs['club_code'] = clubs['club_code'].astype('string')    # @Todo Choose if it is to maintain or not!
+    clubs = clubs.drop(columns=['coach_name', 'total_market_value', 'club_code'])    # Both are null
     clubs['name'] = clubs['name'].astype('string')
-    clubs['domestic_competition_id'] = clubs['domestic_competition_id'].astype('string')    # Here as a Foreign Key!
+    clubs['domestic_competition_id'] = clubs['domestic_competition_id'].astype('string')
+    clubs.rename(columns={'name': 'club_name', 'domestic_competition_id': 'local_competition_code', 'url': 'club_url'})
     # @Todo resolve this:
-    # There are players with 'current_club_id' set on clubs that have recorded 0 players in their squad!
+    # - There are players with 'current_club_id' set on clubs that have recorded 0 players in their squad!
+    players = get_players()
+    clubs['squad_size'] = clubs.apply(lambda x: search_squad_players(x, players))
+    players = None
+
     clubs['stadium_name'] = clubs['stadium_name'].astype('string')
     clubs.at[409, 'stadium_seats'] = 4851       # correcting the only 0 value!
 
     clubs['url'] = clubs['url'].astype('string')
     # @Todo still everything WIP here!
     return clubs
+
+
+def search_squad_players(x, players):
+    if x['squad_size'] == 0:
+        x['squad_size'] = players.query('current_club_id == @x.club_id').shape[0]
+    return x
